4f41db0050ad35e4768ddc116ffaa6e534f78fc6
z88a <zhangyao199503@gmail.com>
2020-06-10 21:46:59 +0800

    add get_sample

---
+++ get_sample.py
@@ -0,0 +1,31 @@
+from grabscreen import grab_screen
+from get_key import key_check
+import cv2
+import time
+import win32con
+
+WINDOWS_SIZE = (0, 40, 800, 600)
+FIG_SIZE = (200, 125)
+
+PAUSE = False
+last_time = time.time()
+while True:
+    if PAUSE:
+        break
+    screen0 = grab_screen(region=WINDOWS_SIZE)
+    screen1 = cv2.cvtColor(screen0, cv2.COLOR_BGR2GRAY)
+    screen2 = cv2.resize(screen1, FIG_SIZE)
+    grab_time = time.localtime(time.time())
+    grab_time1 = time.strftime("%Y%m%d%H%M%S", grab_time)
+    keys = key_check()
+    str_keys =''
+    for item in keys :
+        str_keys = str_keys + str(item)
+    cv2.imshow('window', screen1)
+    cv2.waitKey(1)
+    cv2.imwrite('F:/data/' + grab_time1 + '_' + str_keys + '.png', screen2)
+    print(str_keys)
+    print('time:{}'.format(time.time() - last_time))
+    last_time = time.time()
+    if win32con.VK_ESCAPE in keys:
+        PAUSE = True
